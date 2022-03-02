955a29d943e71bc2dccff474b09858b8be1cb86d
Javier González <doniagv@gmail.com>
2022-03-02 11:14:37 -0600

    Revert "Mul"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from "./logo.svg";
-import "./App.css";
-
-import Addition from "./components/addition/Addition";
-import Subs from "./components/subs/Subs";
-import Mul from "./components/mul/Mul";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Demo Calculadora con Reactjs - Javier Andoni</p>
-        <Addition />
-        <Subs />
-        <Mul />
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        <p>
+          Edit <code>src/App.js</code> and save.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Learn React
         </a>
       </header>
     </div>
   );
+
 }
 
 export default App;
--- src/components/subs/Subs.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Subs = () => {
-  return <h1> (-) </h1>;
-};
-
-export default Subs;
