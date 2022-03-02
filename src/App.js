ed32525448fb8fbfd2412501fa919f29c99dd555
Doniagv <doniagv@gmail.com>
2022-03-02 10:29:37 -0600

    Close #1

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Demo Calculadora</p>
+        <p>Demo Calculadora con Reactjs</p>
         <Addition />
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
